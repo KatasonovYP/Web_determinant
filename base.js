d94688ac12d176df8b018cf7d8ca84d8e0a5aa23
KatasonovYP <katasonov-yury@yandex.ru>
2021-10-30 23:19:27 +0300

    determinate

---
+++ base.js
@@ -0,0 +1,22 @@
+let header = document.createElement('header')
+let h1 = document.createElement('h1')
+h1.innerText = 'Определитель матрицы'
+header.appendChild(h1)
+document.body.appendChild(header)
+
+
+let matrix = document.createElement('div')
+matrix.classList.add('matrix')
+for(let i = 0; i < 9; i++) {
+    let cell = document.createElement('div')
+    cell.classList.add('cell')
+    let value = document.createElement('p')
+    value.classList.add('value')
+    value.innerText = i
+    cell.appendChild(value)
+    matrix.appendChild(cell)
+}
+let content = document.createElement('div')
+content.classList.add('content')
+content.appendChild(matrix)
+document.body.appendChild(content)
